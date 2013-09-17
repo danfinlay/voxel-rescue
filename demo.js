9458002d11d65bf1c618e902be1b91ea4eac16e0
Dan Finlay <dan@danfinlay.com>
2013-09-16 23:58:00 -0700

    Tried fixing demo, still not working, but the module works w/ current voxel-engine.

--- demo.js
+++ demo.js
@@ -1,37 +1,28 @@
-/*jshint asi:true */
-
-var createGame = require('voxel-engine')
-var texturePath = require('painterly-textures')(__dirname)
 var rescue = require('./')
+var voxel = require('voxel')
+var engine = require('voxel-engine')
+var texturePath = require('painterly-textures')(__dirname)
 
-var container = document.querySelector('#container')
+var settings = {
+    generate: voxel.generator['Valley'],
+    chunkDistance: 2,
+    materials: [
+    ['grass', 'dirt', 'grass_dirt'],
+    'obsidian',
+    'brick',
+    'grass'
+    ],
+    texturePath: texturePath,
+    worldOrigin: [0, 0, 0],
+    controls: { discreteFire: true },
+   avatarInitialPosition: [2, 20, 2]
+}
 
-var game = createGame({
-  startingPosition: [0, 1000, 0],
-  texturePath: texturePath,
-  materials: [['grass', 'dirt', 'grass_dirt'], 'bedrock'],
-  generate: function(x,y,z) {
-    if(y === 0 && x > -4 && x < 4 && z > -4 && z < 4) { return 1; }
-    if(y === -20 && x > -20 && x < 20 && z > -20 && z < 20) { return 2; }
-    return 0;
-  }
-})
-
-window.game = game // for debugging
-game.controls.pitchObject.rotation.x = -1.5 // look down
-game.appendTo(container)
-game.currentMaterial = 1
+var game = engine(settings);
 
 var rescuer = rescue(game, {
   frequency: 100
 });
 
-rescuer.on('rescue', function(position) {
-  console.log('Rescued player from position:')
-  console.log(position)
-})
-
-container.addEventListener('click', function() {
-  game.requestPointerLock(container)
-})
-
+// for debugging
+window.game = game
